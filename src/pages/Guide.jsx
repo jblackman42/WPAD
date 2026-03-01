27446b3abb39f40f29ba0071a74fe38a5034dd7e
jblackman42 <jblackman@pureheart.org>
2026-03-01 12:37:21 -0700

    march prayer guide

--- src/pages/Guide.jsx
+++ src/pages/Guide.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import { Loading } from "../components";
 
-const currentGuidePath = '/guide/Feb 2026.pdf';
+const currentGuidePath = '/guide/Mar 2026.pdf';
 
 const Guide = () => {
   useEffect(() => {
